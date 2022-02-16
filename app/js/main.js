06e86ae1662947efe9d8ec2ba537abd5a6bb9319
gordeu <shiddien@gmail.com>
2022-02-16 19:56:32 +0200

    homework number 12

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+
+  $('.slider__inner').slick({
+    infinite: false
+  }); 
+
+  var mixer = mixitup('.top__product-cards');
+
+});
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+  const button = document.querySelector('.catalog') 
+  const nav = document.querySelector('.catalog__content') 
+  const btn = document.querySelector('.catalog__btn')
+  button.addEventListener('click', () => { 
+    nav.classList.toggle('catalog__content--active') 
+    btn.classList.toggle('catalog__btn--active')
+  })
+  window.addEventListener('click', e => { 
+    const target = e.target 
+    if (!target.closest('.catalog__content') && !target.closest('.catalog')) {  
+      nav.classList.remove('catalog__content--active') 
+      btn.classList.remove('catalog__btn--active')
+    }
+  })
+})
